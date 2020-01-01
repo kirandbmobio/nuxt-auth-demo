a35930173d46839f997b19878f650766f33e1381
kirandbmobio <kiranb.mobio@gmail.com>
2020-01-01 11:40:51 +0530

    port change

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,76 +1,78 @@
 module.exports = {
-    mode: 'universal',
-    /*
-     ** Headers of the page
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-    /*
-     ** Customize the progress-bar color
-     */
-    loading: { color: '#fff' },
-    /*
-     ** Global CSS
-     */
-    css: [
-
+  mode: "universal",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    server: {
-        port: 4000
-    },
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  server: {
+    port: 3000
+  },
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/axios",
+    "@nuxtjs/auth"
+  ],
+  /*
+   ** Build configuration
+   */
+  build: {
     /*
-     ** Plugins to load before mounting the App
+     ** You can extend webpack config here
      */
-    plugins: [],
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://bootstrap-vue.js.org
-        'bootstrap-vue/nuxt',
-        '@nuxtjs/axios',
-        '@nuxtjs/auth'
-    ],
-    /*
-     ** Build configuration
-     */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {}
-    },
-    env: {
-        baseUrl: process.env.BASE_URL || 'http://localhost:3000/api'
-    },
-    axios: {
-        baseURL: 'http://localhost:3000/api'
-    },
-    auth: {
-        strategies: {
-            local: {
-                endpoints: {
-                    login: { url: '/auth/login', method: 'post', propertyName: 'token'},
-                    // signup: { url: '/auth/register', method: 'post', propertyName: 'token'}
-                    user: { url: '/auth/me', method: 'get', propertyName: 'user'}
-                },
-                tokenRequired: true,
-                tokenName: 'Authorization',
-                tokenType: 'Bearer'
-            }
-        }
+    extend(config, ctx) {}
+  },
+  /* set baseurl of backend here */
+  env: {
+    baseUrl: process.env.BASE_URL || "http://localhost:4000/api"
+  },
+  axios: {
+    baseURL: "http://localhost:4000/api"
+  },
+  /* token base authentication  route */
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: "/auth/login", method: "post", propertyName: "token" },
+          // signup: { url: '/auth/register', method: 'post', propertyName: 'token'}
+          user: { url: "/auth/me", method: "get", propertyName: "user" }
+        },
+        tokenRequired: true,
+        tokenName: "Authorization",
+        tokenType: "Bearer"
+      }
     }
-}
+  }
+};
